1972657284f1ce68d8bfff10c21991911bf3060f
MrOJ <251362783@qq.com>
2015-03-04 13:51:41 +0800

    Version 0.5.0

---
+++ GreenTrip/GreenTrip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GreenTrip
+//
+//  Created by Mr.OJ on 15/3/4.
+//  Copyright (c) 2015年 Mr.OJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GreenTrip/GreenTrip/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  GreenTrip
+//
+//  Created by Mr.OJ on 15/3/4.
+//  Copyright (c) 2015年 Mr.OJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
